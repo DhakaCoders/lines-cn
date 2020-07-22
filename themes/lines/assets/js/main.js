f0a999a4ebd9b701687763900fe616c3116922a5
Rannojit <cserannojitsarkar22@gmail.com>
2020-07-22 21:10:32 +0600

    increment and decrement add

--- themes/lines/assets/js/main.js
+++ themes/lines/assets/js/main.js
@@ -251,5 +251,38 @@ $('.enter-now-btn').on('click', function(e){
 });
 
 
+$('.qty').each(function() {
+  var spinner = $(this),
+    input = spinner.find('input[type="number"]'),
+    btnUp = spinner.find('.plus'),
+    btnDown = spinner.find('.minus'),
+    min = 1,
+    max = input.attr('max');
+
+  btnUp.click(function() {
+    var oldValue = parseFloat(input.val());
+    if (oldValue <= max) {
+      var newVal = oldValue;
+    } else {
+      var newVal = oldValue + 1;
+    }
+    spinner.find("input").val(newVal);
+    spinner.find("input").trigger("change");
+  });
+
+  btnDown.click(function() {
+    var oldValue = parseFloat(input.val());
+    if (oldValue <= min) {
+      var newVal = oldValue;
+    } else {
+      var newVal = oldValue - 1;
+    }
+    spinner.find("input").val(newVal);
+    spinner.find("input").trigger("change");
+  });
+
+});
+
+
 
 })(jQuery);
