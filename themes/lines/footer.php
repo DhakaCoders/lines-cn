fe1ebecacafd836a735920faa99d75db67005a26
azizulsoftsw <azizul.softsw@gmail.com>
2020-08-04 13:05:47 +0600

    init

---
+++ themes/lines/footer.php
@@ -0,0 +1,76 @@
+  <footer class="footer-wrp">
+    <div class="footer-top-sec">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3 col-md-12">
+            <div class="ftr-col">
+              <div class="ftr-logo">
+                <a href="#">
+                  <img src="<?php echo THEME_URI; ?>/assets/images/footer-logo.png">
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4">
+            <div class="ftr-col ftr-visit-col">
+              <h5 class="ftr-col-title">VISIT OUR SHOP</h5>
+              <p>For pick-up or deli
+              very beer and merch, please visit our SHOP Please note we are only delivering beer to Phoenix and East Valley with a $30 minimum order!  </p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4">
+            <div class="ftr-col">
+              <h5 class="ftr-col-title">CONTACT US</h5>
+              <p><span>Gilbert Brewpub:</span> <a href="#">480-497-2739</a> DTPHX: <a href="#">480-462-1836</a> For general inquiries: <a href="#">info@azwbeer.com</a> For keg inquiries: <a href="#">konrad@azwbeer.com</a></p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4">
+            <div class="ftr-col">
+              <h5 class="ftr-col-title">CURRENT INITIATIVES</h5>
+              <p>In our efforts to become a more cognizant and sustainable business, we’ve partner with Recycled City who’s vision of “Farmland for the Future” is som
+              ething we passionately support! </p>
+              <p>100% of the food waste collected by Recycled City goes towards building local-fertile farmland.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> 
+
+    <div class="footer-btm-con-wrap">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12">
+              <div class="footer-btm-con">
+                <div class="ftr-socila">
+                  <ul class="clearfix reset-list"> 
+                    <li>
+                      <a class="ftr-social-instagram" href="#">
+                        <i class="fab fa-instagram"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a class="ftr-social-facebook" href="#">
+                        <i class="fab fa-facebook-square"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a class="ftr-social-twitter" href="#">
+                        <i class="fab fa-twitter-square"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <div class="ftr-copyright-text">
+                  <p>Lines Brew Co. © 2020 All rights reserved.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  </footer>
+</div><!-- end of home page sections controller div -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
